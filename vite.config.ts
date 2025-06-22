df08cb3e9dfc6811b2caaa7dfa04d57b8c1c3381
Jonathan Aguirre <prismaymedia@gmail.com>
2025-06-22 05:07:53 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url); // ES module __filename equiv
 const __dirname = path.dirname(__filename); // ES module __dirname equivalent
 
 export default defineConfig({
-  base: '/', // Change base path to root
+  // base: '/linkfy/', // Revert base path for GitHub Pages repository site
   plugins: [
     react(),
     runtimeErrorOverlay(),

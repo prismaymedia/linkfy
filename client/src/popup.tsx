2b67515ff8e2bb0cf5e1720c3cdeb215087f4523
Daniela Aguirre <de4519280@gmail.com>
2025-09-10 15:18:30 -0500

    bug(extension): Conditionally load i18n to avoid MV3 conflicts

--- client/src/popup.tsx
+++ client/src/popup.tsx
@@ -1,7 +1,17 @@
-import './i18n';
-
 import { createRoot } from 'react-dom/client';
 import AppExtension from './AppExtension';
 import './index.css';
+
+// We declare chrome as a global variable to avoid reference errors
+declare const chrome: any;
+
+// We initialize i18n conditionally if needed
+if (
+  typeof window !== 'undefined' &&
+  typeof chrome !== 'undefined' &&
+  chrome.runtime
+) {
+  import('./i18n');
+}
 
 createRoot(document.getElementById('root')!).render(<AppExtension />);
